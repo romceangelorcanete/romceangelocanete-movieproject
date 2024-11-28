6f43f07922683d617a1ef6740f99a0647fe7e84b
Romce Angelo R. Cañete <romceangelorcanete>
2024-11-28 20:17:27 +0800

    added design Main.css and .jsx

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,38 +1,69 @@
-import { useEffect } from 'react';
+import { useEffect, useContext, useCallback } from 'react';
+import { AuthContext } from '../../context/context';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt, faFilm, faTachometerAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import './Main.css';
 
 function Main() {
-  const accessToken = localStorage.getItem('accessToken');
+  //get user info
+  const { auth } = useContext(AuthContext);
   const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem('accessToken');
+
+  const { clearAuthData } = useContext(AuthContext);
+
+
+  const handleResetTab = () => {
+    localStorage.setItem('tab', JSON.stringify('cast'));
+  }
+
+  const handleLogout = useCallback(() => {
+    clearAuthData();
     navigate('/');
-  };
+  }, [navigate, clearAuthData]);
 
   useEffect(() => {
-    if (
-      accessToken === undefined ||
-      accessToken === '' ||
-      accessToken === null
-    ) {
+    if (!auth.accessToken) {
       handleLogout();
     }
-  }, []);
+  }, [auth.accessToken, handleLogout]);
+
   return (
-    <div className='Main'>
-      <div className='container'>
-        <div className='navigation'>
-          <ul>
+    <div className="Main">
+      <div className="custom-container">
+        <div className="navigation text-light">
+        <h1 className="app-title">+NETMOVIES</h1> {/* Added +NETMOVIES */}
+          <div className="admin-info">
+            <FontAwesomeIcon icon={faUserCircle} style={{ fontSize: '50px', color: 'white' }} />
+            <span className="user-info">
+              <p className="role">{auth.user.role}</p>
+              <h1 className="name">{auth.user.firstName}</h1>
+            </span>
+          </div>
+          <hr></hr>
+          <ul className="nav">
             <li>
-              <a href='/main/dashboard'>Dashboard</a>
+              <a href="/main/dashboard" className="nav-link" title="Dashboard">
+                <center>
+                  <FontAwesomeIcon icon={faTachometerAlt} style={{ fontSize: '24px', color: 'white' }} />
+                </center>
+              </a>
             </li>
-            <li className='logout'>
-              <a onClick={handleLogout}>Logout</a>
+            <li>
+              <a href="/main/movies" className="nav-link" title="Movies" onClick={handleResetTab}>
+                <center>
+                  <FontAwesomeIcon icon={faFilm} style={{ fontSize: '24px', color: 'white' }} />
+                </center>
+              </a>
+            </li>
+                        <li className="logout" title="Logout">
+              <button onClick={handleLogout} className="nav-link logout-btn">
+                <FontAwesomeIcon icon={faSignOutAlt} className="nav-icon" />
+              </button>
             </li>
           </ul>
         </div>
-        <div className='outlet'>
+        <div className="outlet bg-custom text-light">
           <Outlet />
         </div>
       </div>
